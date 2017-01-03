126727e9f751f0485ebd1c8513396570b8fb20dd
webee.yw <webee.yw@gmail.com>
2017-01-03 16:52:20 +0800

    fix test/beta log settings.

--- src/xchat_site/config/settings/beta.py
+++ src/xchat_site/config/settings/beta.py
@@ -1,1 +1,28 @@
 XCHAT_API_ROOT_URL = 'http://xchat.ddcard.com.cn'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'requests': {
+            'level': 'ERROR'
+        }
+    },
+}
--- src/xchat_site/config/settings/test.py
+++ src/xchat_site/config/settings/test.py
@@ -3,3 +3,30 @@ import os
 STATIC_ROOT = os.path.join('/data/xchat', 'static')
 
 XCHAT_API_ROOT_URL = 'http://t.xchat.engdd.com'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'requests': {
+            'level': 'ERROR'
+        }
+    },
+}
